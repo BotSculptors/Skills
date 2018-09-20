72e672550463c826abbd1a191d39a7e54627962e
Surik266 <surik.pozoyan@gmail.com>
2018-09-20 09:46:43 +0400

    add routes

---
+++ src/components/responsiveDesign.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import BtnNext from './btn/btnNext'
+
+
+class Design extends Component {
+  render() {
+    return (
+
+      <div >
+
+        <span>Design</span>
+        <BtnNext />
+      </div>
+
+    );
+  }
+}
+
+export default Design;
--- src/containers/question.js
+++ src/containers/question.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 
 import Name from '../components/Name';
+import Design from '../components/responsiveDesign';
 
 class question extends Component {
   render() {
     return (
 
       <div className="question">
+        <BrowserRouter>
+        <Switch>
+              <Route exact path='/' render={() => <Name />} />
+            <Route exact path='/design' render={() => <Design />} />
 
-        <Name/>
-        
+
+      </Switch>
+      </BrowserRouter>
       </div>
     );
   }
